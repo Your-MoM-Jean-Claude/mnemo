881a83efa76fea93fce8e0af9cdf2a8b047e30b2
Your-MoM-Jean-Claude <jir.filipec@gmail.com>
2026-05-25 17:43:06 +0800

    Update sign.sh

--- sign.sh
+++ sign.sh
@@ -3,12 +3,16 @@
   keychain initialize
   echo "$CM_CERTIFICATE" | base64 --decode > /tmp/cert.p12
   KEYCHAIN=$(security default-keychain | xargs)
-  security import /tmp/cert.p12 -k "$KEYCHAIN" -P "$CM_CERTIFICATE_PASSWORD" -T
-  /usr/bin/codesign -A
+  security import /tmp/cert.p12 \
+    -k "$KEYCHAIN" \
+    -P "$CM_CERTIFICATE_PASSWORD" \
+    -T /usr/bin/codesign \
+    -A
   PROFILES_DIR="$HOME/Library/MobileDevice/Provisioning Profiles"
   mkdir -p "$PROFILES_DIR"
-  echo "$PROVISIONING_PROFILE" | base64 --decode >
-  "$PROFILES_DIR/mnemo.mobileprovision"
-  PROFILE_UUID=$(security cms -D -i "$PROFILES_DIR/mnemo.mobileprovision" 2>/dev/null
-  | plutil -extract UUID raw -)
+  echo "$PROVISIONING_PROFILE" | base64 --decode \
+    > "$PROFILES_DIR/mnemo.mobileprovision"
+  PROFILE_UUID=$(security cms -D \
+    -i "$PROFILES_DIR/mnemo.mobileprovision" \
+    2>/dev/null | plutil -extract UUID raw -)
   echo "Profile UUID: $PROFILE_UUID"
